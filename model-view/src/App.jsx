c661550bd81abc377ef2f0e5c004cabfb385651e
Shope Alli-B <zoeyalli921@gmail.com>
2026-01-31 16:29:09 -0700

    first commit

--- model-view/src/App.jsx
+++ model-view/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ProtectedRoute from './routes/ProtectedRoute';
 import Dashboard from './pages/Dashboard';
 import Transaction from './pages/Transaction';
 import Budget from './pages/Budget';
@@ -18,7 +19,8 @@ const App = () => {
         <Route path="/" element={<Register />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<ProtectedRoute>
+              <Dashboard /></ProtectedRoute>} />
         <Route path="/transaction" element={<Transaction />} />
         <Route path="/budget" element={<Budget />} />
         <Route path="/analytics" element={<Analytics />} />
--- model-view/src/Routes/ProtectedRoutes.jsx
+++ model-view/src/Routes/ProtectedRoutes.jsx
@@ -1,9 +1,11 @@
 import { Navigate } from 'react-router-dom';
 import { isTokenExpired } from '../utils/token';
+import { toast } from 'react-toastify';
 
 const ProtectedRoute = ({ children }) => {
   if (isTokenExpired()) {
     localStorage.removeItem('token');
+    toast.warning('Please log in to continue');
     return <Navigate to="/login" replace />;
   }
   return children;
