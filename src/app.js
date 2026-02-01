8c0cd38d7a0c4863d2a44c2b17776e548ccabd25
Shope Alli-B <zoeyalli921@gmail.com>
2026-02-01 16:08:58 -0700

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -9,22 +9,17 @@ const analyticsRoutes = require('./routes/analyticsRoutes');
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://finance-tracker-wheat-zeta.vercel.app'
-];
+const corsOptions = {
+  origin: [
+    'https://finance-tracker-wheat-zeta.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
-
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
