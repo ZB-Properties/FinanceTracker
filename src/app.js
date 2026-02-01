6b2e8d517d6560657f4c8611f37b60bfe0032b0d
Shope Alli-B <zoeyalli921@gmail.com>
2026-02-01 12:32:45 -0700

    Fix CORS preflight for Vercel frontend

--- src/app.js
+++ src/app.js
@@ -20,8 +20,13 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 app.use(express.json());
 
