39edd9b47d122c121fe1f067fcf5ef286c439931
Shope Alli-B <zoeyalli921@gmail.com>
2026-02-01 15:59:55 -0700

    Fix CORS

--- src/app.js
+++ src/app.js
@@ -14,22 +14,16 @@ const allowedOrigins = [
   'https://finance-tracker-wheat-zeta.vercel.app'
 ];
 
-const corsOptions = {
-  origin: function(origin, callback) {
+
+app.use(cors({
+  origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  },
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('', cors(corsOptions)); 
-
+  }
+}));
 
 
 app.use(express.json());
