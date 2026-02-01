7e0f707aba7152d413476ab5b1dad7aaeb564038
Shope Alli-B <zoeyalli921@gmail.com>
2026-02-01 09:51:16 -0700

    Fix CORS for Vercel frontend

--- src/app.js
+++ src/app.js
@@ -16,9 +16,13 @@ app.use(cors(
     'http://localhost:5173' 
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
 }
 ));
+
+app.options('*', cors());
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
